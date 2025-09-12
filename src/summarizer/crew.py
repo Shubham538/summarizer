d8b417f96de4d62dc0882a426ef11f142396f654
Shubham538 <bansal.shubham002@gmail.com>
2025-09-12 16:37:35 +0530

    Initial commit

---
+++ src/summarizer/crew.py
@@ -0,0 +1,39 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from typing import List
+from pypdf import PdfReader  
+from crewai_tools import SerperDevTool
+from pathlib import Path
+
+@CrewBase
+class Summarizer():
+    """Summarizer crew"""
+
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
+
+    @agent
+    def researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config['researcher'],
+            verbose=True
+        )
+
+    @task
+    def research_updates(self) -> Task:
+        return Task(
+            config=self.tasks_config['research_updates'],
+            output_file='research_findings.txt'
+        )
+
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the Summarizer crew"""
+
+        return Crew(
+            agents=self.agents,
+            tasks=self.tasks,
+            process=Process.sequential,
+            verbose=True,
+        )
---
+++ src/summarizer/main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import sys
+from pathlib import Path
+import warnings
+from summarizer.crew import Summarizer
+from dotenv import load_dotenv
+import pdfplumber
+import gradio as gr
+import tiktoken
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+load_dotenv(override=True)
+
+def extract_pdf(pdf_path, output_file="extracted_text.txt"):
+    text = []
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text.append(page.extract_text())
+    final_text = "\n".join(filter(None, text))
+    Path(output_file).write_text(final_text, encoding="utf-8")
+    return final_text
+
+def count_tokens(text: str, model: str = "gpt-4o-mini") -> int:
+    """
+    Estimate number of tokens in the given text for a specific model.
+    Default: gpt-4o-mini.
+    """
+    enc = tiktoken.encoding_for_model(model)
+    return len(enc.encode(text))
+
+def run(base_name: str):
+    """
+    Run the crew.
+    """
+    BASE_DIR = Path(__file__).resolve().parent.parent  # adjust to your project root
+    file_path = BASE_DIR /"summarizer"/ "inputs" / f"{base_name}.pdf"
+    raw_text = extract_pdf(str(file_path), "extracted_text.txt")
+    
+    print("Estimated Token Count:", count_tokens(raw_text))
+
+    inputs = {
+        'raw_text' : raw_text
+    }
+    
+    crew_instance = Summarizer()
+    result = crew_instance.crew().kickoff(inputs=inputs)
+
+    print("Research completed!!")
+    return result
+
+
